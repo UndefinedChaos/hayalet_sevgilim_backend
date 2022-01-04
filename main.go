4244d0cdc5f90e65925236559d46bf9fe5459369
Anıl Arıkan <anilarikan@Anl-MacBook-Air.local>
2022-01-04 18:04:00 +0300

    refactor

--- main.go
+++ main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -22,65 +20,39 @@ type Response struct {
 	Message       string
 }
 
-type CounterFile struct {
-	Counter int `json:"counter"`
-}
-
 var router = mux.NewRouter()
 
 func YourHandler(w http.ResponseWriter, r *http.Request) {
-	//Counter check
-	dir, err := os.Getwd()
+
+	lat := r.FormValue("lat")
+	long := r.FormValue("long")
+	lat_long := lat + "," + long
+	url := "https://api.foursquare.com/v3/places/search?query=starbucks&ll=" + lat_long + "&radius=50&sort=DISTANCE&limit=1"
+
+	req, _ := http.NewRequest("GET", url, nil)
+
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("Authorization", "fsq3IpOjjwcDiE17Itn4wFCJmSRC3TdlI7JsaiJzdh+kLV8=")
+	var response = Response{}
+	res, err := http.DefaultClient.Do(req)
 	if err != nil {
-		log.Fatal(err)
+		w.WriteHeader(http.StatusNotFound)
+		response.Message = "Not Found"
 	}
-	fmt.Println(dir)
-	counterFile, err := os.Open(dir + "/counter.json")
-	if err != nil {
-		log.Fatal("NewRequest: ", err)
-		return
-	}
-	defer counterFile.Close()
-	var counter CounterFile
-	json.NewDecoder(counterFile).Decode(&counter)
-	//
-	if counter.Counter < 35000 {
-
-		lat := r.FormValue("lat")
-		long := r.FormValue("long")
-		lat_long := lat + "," + long
-		url := "https://api.foursquare.com/v3/places/search?query=starbucks&ll=" + lat_long + "&radius=50&sort=DISTANCE&limit=1"
-
-		req, _ := http.NewRequest("GET", url, nil)
-
-		req.Header.Add("Accept", "application/json")
-		req.Header.Add("Authorization", "fsq3IpOjjwcDiE17Itn4wFCJmSRC3TdlI7JsaiJzdh+kLV8=")
-
-		res, err := http.DefaultClient.Do(req)
-		if res == nil {
-			log.Fatal("NewRequest: ", err)
-			return
-		}
-		defer res.Body.Close()
-		//Counter increasing
-
-		counter.Counter = counter.Counter + 1
-		file, _ := json.Marshal(counter)
-		_ = ioutil.WriteFile(dir+"/counter.json", file, 0644)
-
-		//
+	defer res.Body.Close()
+	if res.StatusCode >= 200 && res.StatusCode <= 299 {
 		var record myInfo
 		json.NewDecoder(res.Body).Decode(&record)
 		w.Header().Set("Content-Type", "application/json")
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		var response = Response{}
+
 		if len(record.Arr) > 0 {
 			response.LocationFound = true
 			response.Message = "Starbucks Bulundu"
 			js, _err := json.Marshal(response)
 			if _err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
+				w.WriteHeader(http.StatusInternalServerError)
+				response.Message = "Server Error"
 			}
 			w.Write(js)
 		} else {
@@ -88,25 +60,22 @@ func YourHandler(w http.ResponseWriter, r *http.Request) {
 			response.Message = "Starbucks Bulunamadı"
 			js, _err := json.Marshal(response)
 			if _err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
+				w.WriteHeader(http.StatusInternalServerError)
+				response.Message = "Server Error"
 			}
 			w.Write(js)
 		}
 	} else {
-		w.Header().Set("Content-Type", "application/json")
-		var response = Response{}
+		w.WriteHeader(http.StatusNotFound)
 		response.LocationFound = false
-		response.Message = "Foursquare request limit achieved"
+		response.Message = "API limit achieved"
 		js, _err := json.Marshal(response)
 		if _err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
+			w.WriteHeader(http.StatusInternalServerError)
+			response.Message = "Server Error"
 		}
 		w.Write(js)
-
 	}
-
 }
 
 func main() {
